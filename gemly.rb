ad7b547f9e00b8717618ea5101ecea241c941d18
Fernando Ortiz <fortiz2k@gmail.com>
2012-01-25 01:29:39 -0600

    fix undefined methods and a system stack error.

--- gemly.rb
+++ gemly.rb
@@ -3,23 +3,19 @@ require_relative 'main'
 class Gemly < OpenStruct
   APIURL = 'http://rubygems.org/api/v1/gems/'
 
-  def parse(raw)
-    gemly = new pull_spec_for raw
+  def self.parse(raw)
+    gemly = new pull_spec_from raw
     puts "#{gemly.name} was retrieved!"
   end
 
-  def pull_spec_from(raw)
+  def self.pull_spec_from(raw)
     name = raw.split.first
-    begin
-      url = APIURL + name + '.json'
-      body = Curl::Easy.http_get(json_url).body_str
-      @spec = Yajl::Parser.parse body, symbolize_keys: true
-    rescue
-      pull_spec_from raw
-    end
+    url = APIURL + name + '.json'
+    body = Curl::Easy.http_get(url).body_str
+    @spec = Yajl::Parser.parse body, symbolize_keys: true
   end
 
-  def attributes
+  def self.attributes
     marshal_dump
   end
 end
